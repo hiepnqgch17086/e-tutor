62186a587279425443d6d2d1c805e5898fc45dfe
hiep294 <quanghiep294@gmail.com>
2020-04-11 09:24:36 +0700

    ss

--- src/pages/MeetingListPage/ListOfMeeting/MeetingItem/index.tsx
+++ src/pages/MeetingListPage/ListOfMeeting/MeetingItem/index.tsx
@@ -10,24 +10,21 @@ import { IS_TUTOR } from '../../../../models-one-prop/role'
 import { observer } from 'mobx-react-lite'
 
 const MeetingItem = ({ item = defaultOfMeeting }) => {
-  // const item = {
-  //   title: "Meeting title",
-  //   with: "Std/tutor one",
-  //   id: 1,
-  //   name: 'Name tutor/student',
-  //   avatar: '',
-  //   startAt: moment().format(),
-  //   endAt: moment().format()
-  // }
+
+  const { currentUser } = ProfilePageData
+  const partner = currentUser.id === item.studentId.id
+    ? item.creatorId
+    : item.studentId
+
   return (
     <div className="calendar-events mb-2 d-flex border-top" data-class="bg-success"
       style={{ cursor: 'pointer' }}
     >
       <a href={get_MEETING_DETAIL_PAGE(item.id)} className="message-item d-flex align-items-center flex-grow-1" target="_blank" rel="noopener noreferrer">
         <div className="d-flex align-items-center mr-auto">
-          <img src={item.studentId.avatar || AvatarInDefault} alt="user" className="rounded-circle" width={40} height={40} />
+          <img src={partner.avatar || AvatarInDefault} alt="user" className="rounded-circle" width={40} height={40} />
           <div className="w-75 d-inline-block v-middle pl-2">
-            <h6 className="message-title mb-0 mt-1">{item.studentId.name}</h6>
+            <h6 className="message-title mb-0 mt-1">{partner.name}</h6>
             <span className="font-12 text-nowrap d-block text-dark">Title: {item.title}</span>
             <span className="font-12 text-nowrap d-block text-muted">
               {'Start At: ' + moment(item.startAt).format('LT')}
